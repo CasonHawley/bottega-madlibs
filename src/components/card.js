e784fd5491103979fab2937a76bd37fa09411c1d
casonhawley <cason.hawley@gmail.com>
2018-11-30 13:44:14 -0700

    re-positioned the generate button, and fixed content counter

--- src/components/card.js
+++ src/components/card.js
@@ -27,11 +27,12 @@ class Card extends Component {
 
     constructor() {
         super()
+
         this.state = INITIAL_STATE;
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    }
+    } 
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value })
@@ -42,19 +43,19 @@ class Card extends Component {
 
         if(this.state.contentVisible) {
             this.setState(INITIAL_STATE)
-
         } else {
             this.setState({ contentVisible: true })
         }
     }
 
     render() {
+
         const inputData = [
             {title: 'Color', state: this.state.color, name: 'color'},
             {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
             {title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
             {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
-            
+
             {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
             {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
             {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
@@ -71,17 +72,16 @@ class Card extends Component {
             {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'},
         ]
 
-
         return (
             <form onSubmit={this.handleFormSubmit} className="card">
                 <div className="card__inputs">
-                    {
-                        inputData.map((data, index) => {
-                            return Input( (data), this.handleInputChange, index )
-                        })
-                    }
+                {
+                    inputData.map((data, index) => {
+                        return Input( (data), this.handleInputChange, index) 
+                    })
+                }
                 </div>
-                <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form' }</button>
+                <button className={`card__${!this.state.contentVisible ? 'generate' : 'clear'}`} type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
                 {
                     this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
